c45fd3964b32967ae582c40d0afa6ef513c836b2
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-06-15 19:06:12 -0400

    Added more strings classifiers items for this package

--- setup.py
+++ setup.py
@@ -1,23 +1,41 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '0.7'
 
 setup(name='plonetheme.motion',
       version=version,
-      description="",
+      description="Motion, is an installable Diazo theme for Plone 4",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
+      # https://pypi.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: Theme",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='diazo theme',
+      keywords='web zope plone theme diazo motion',
       author='Davi Lima',
       author_email='davi@simplesconsultoria.com.br',
-      url='http://svn.plone.org/svn/collective/plonetheme.motion',
+      maintainer='Leonardo Caballero',
+      maintainer_email='leonardocaballero@gmail.com',
+      url='https://github.com/davilima6/plonetheme.motion',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plonetheme'],
